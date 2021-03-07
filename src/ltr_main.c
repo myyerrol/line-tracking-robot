25ae855ee260e0101b4140301fc165e54d183430
myyerrol <myyerrol@126.com>
2021-03-07 22:42:12 +0800

    Update some files

--- src/ltr_main.c
+++ src/ltr_main.c
@@ -4,10 +4,6 @@ int main(void) {
     // pinMode(11, OUTPUT);
 
     while (1) {
-        // digitalWrite(11, HIGH);
-        // delay(1000);
-        // digitalWrite(11, LOW);
-        // delay(1000);
         for (uint8_t i = 0; i < 255; i++) {
             analogWrite(11, i);
             delay(10);
