97f72d3110596c02808b4ed2f5c006b22e4c457b
myyerrol <myyerrol@126.com>
2021-03-07 20:49:01 +0800

    Update to 21.03.07

--- lib/RVStarToArduino/src/RVStarToArduino.c
+++ lib/RVStarToArduino/src/RVStarToArduino.c
@@ -1,6 +1,6 @@
 #include "RVStarToArduino.h"
 
-static const uint32_t RVSTAR_GPIO_CLK_ARR[] = {
+static const uint32_t GPIO_CLK_ARR[] = {
     RCU_GPIOC,
     RCU_GPIOC,
     RCU_GPIOC,
@@ -22,7 +22,7 @@ static const uint32_t RVSTAR_GPIO_CLK_ARR[] = {
     RCU_GPIOC,
     RCU_GPIOC
 };
-static const uint32_t RVSTAR_GPIO_PORT_ARR[] = {
+static const uint32_t GPIO_PORT_ARR[] = {
     GPIOC,
     GPIOC,
     GPIOC,
@@ -44,7 +44,7 @@ static const uint32_t RVSTAR_GPIO_PORT_ARR[] = {
     GPIOC,
     GPIOC
 };
-static const uint32_t RVSTAR_GPIO_PIN_ARR[] = {
+static const uint32_t GPIO_PIN_ARR[] = {
     GPIO_PIN_11,
     GPIO_PIN_10,
     GPIO_PIN_8,
@@ -66,7 +66,7 @@ static const uint32_t RVSTAR_GPIO_PIN_ARR[] = {
     GPIO_PIN_4,
     GPIO_PIN_5
 };
-static const uint8_t RVSTAR_GPIO_MODE_ARR[] = {
+static const uint8_t GPIO_MODE_ARR[] = {
     GPIO_MODE_IN_FLOATING, // 浮动输入模式 Floating input mode
     GPIO_MODE_OUT_PP,      // 推挽输出模式 Push-pull output mode
     GPIO_MODE_IPU,         // 上拉输入模式 Pull-up input mode
@@ -184,23 +184,29 @@ void initTimerPWMMode(uint8_t pin) {
     uint32_t timer           = TIMER_ARR[pin];
     uint32_t timer_ch        = TIMER_CH_ARR[pin];
 
+    // 配置定时器端口与备用功能时钟
+    // Configure timer pin and alternate function clock
     rcu_periph_clock_enable(timer_gpio_clk);
     rcu_periph_clock_enable(RCU_AF);
 
+    // 初始化定时器GPIO参数
+    // Initialize timer gpio parameter
     gpio_init(timer_gpio_port, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ,
               timer_gpio_pin);
 
     timer_oc_parameter_struct timer_ocinitpara;
     timer_parameter_struct    timer_initpara;
 
+    // 配置定时器时钟并重置定时器
+    // Configure timer clock and reset timer
     rcu_periph_clock_enable(timer_clk);
     timer_deinit(timer);
 
     // 初始化定时器初始参数结构
-    // Initialize TIMER init parameter struct
+    // Initialize timer init parameter struct
     timer_struct_para_init(&timer_initpara);
-    // 设置定时器初始参数
-    // Set TIMER init parameter
+    // 配置定时器初始参数
+    // Configure timer init parameter
     timer_initpara.prescaler         = 107;
     timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
     timer_initpara.counterdirection  = TIMER_COUNTER_UP;
@@ -210,10 +216,10 @@ void initTimerPWMMode(uint8_t pin) {
     timer_init(timer, &timer_initpara);
 
     // 初始化定时器通道输出参数结构
-    // initialize TIMER channel output parameter struct
+    // Initialize timer channel output parameter struct
     timer_channel_output_struct_para_init(&timer_ocinitpara);
-    // 设置定时器通道输出参数（PWM模式）
-    // Set TIMER channel output parameter(PWD mode)
+    // 配置定时器通道输出参数（PWM模式）
+    // Configure timer channel output parameter(PWM mode)
     timer_ocinitpara.outputstate  = TIMER_CCX_ENABLE;
     timer_ocinitpara.outputnstate = TIMER_CCXN_DISABLE;
     timer_ocinitpara.ocpolarity   = TIMER_OC_POLARITY_HIGH;
@@ -221,19 +227,21 @@ void initTimerPWMMode(uint8_t pin) {
     timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
 
+    // 启动定时器通道输出
+    // Enable timer output
     timer_channel_output_config(timer, timer_ch, &timer_ocinitpara);
 
-    // 设置定时器通道为PWM0模式
-    // Set TIMER channel in PWM0 mode
+    // 配置定时器通道为PWM0模式
+    // Configure timer channel in PWM0 mode
     timer_channel_output_pulse_value_config(timer, timer_ch, 255);
     timer_channel_output_mode_config(timer, timer_ch, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(timer, timer_ch, TIMER_OC_SHADOW_DISABLE);
 
     // 启动定时器自动重新加载功能
-    // enable TIMER auto reload shadow function
+    // Enable timer auto reload shadow function
     timer_auto_reload_shadow_enable(timer);
     // 启动定时器
-    // enable TIMER
+    // Enable timer
     timer_enable(timer);
 }
 
@@ -250,37 +258,38 @@ void analogWrite(uint8_t pin, int val) {
     uint32_t timer_ch   = TIMER_CH_ARR[pin];
     uint32_t timer_flag = TIMER_FLAG_ARR[pin];
 
+    // 如果定时器通道未初始化，则先初始化定时器通道为PWM模式
+    // If timer channel is not initialized, initialize timer channel to PWM mode
     if (timer_flag_get(timer, timer_flag) == RESET) {
         initTimerPWMMode(pin);
     }
 
+    // 配置定时器通道输出脉冲值
+    // Configure timer channel output pulse value
     timer_channel_output_pulse_value_config(timer, timer_ch, (255 - val));
 }
 
 int digitalRead(uint8_t pin) {
-    uint32_t rvstar_gpio_port = RVSTAR_GPIO_PORT_ARR[pin];
-    uint32_t rvstar_gpio_pin  = RVSTAR_GPIO_PIN_ARR[pin];
-    FlagStatus rvstar_flag_status = gpio_input_bit_get(rvstar_gpio_port,
-                                                       rvstar_gpio_pin);
-    return rvstar_flag_status;
+    uint32_t gpio_port = GPIO_PORT_ARR[pin];
+    uint32_t gpio_pin  = GPIO_PIN_ARR[pin];
+    return gpio_input_bit_get(gpio_port, gpio_pin);
 }
 
 void digitalWrite(uint8_t pin, uint8_t val) {
-    uint32_t rvstar_gpio_port = RVSTAR_GPIO_PORT_ARR[pin];
-    uint32_t rvstar_gpio_pin  = RVSTAR_GPIO_PIN_ARR[pin];
-    gpio_bit_write(rvstar_gpio_port, rvstar_gpio_pin, val);
+    uint32_t gpio_port = GPIO_PORT_ARR[pin];
+    uint32_t gpio_pin  = GPIO_PIN_ARR[pin];
+    gpio_bit_write(gpio_port, gpio_pin, val);
 }
 
 void pinMode(uint8_t pin, uint8_t mode) {
-    uint32_t rvstar_gpio_clk  = RVSTAR_GPIO_CLK_ARR[pin];
-    uint32_t rvstar_gpio_port = RVSTAR_GPIO_PORT_ARR[pin];
-    uint32_t rvstar_gpio_pin  = RVSTAR_GPIO_PIN_ARR[pin];
-    uint8_t  rvstar_gpio_mode = RVSTAR_GPIO_MODE_ARR[mode];
+    uint32_t gpio_clk  = GPIO_CLK_ARR[pin];
+    uint32_t gpio_port = GPIO_PORT_ARR[pin];
+    uint32_t gpio_pin  = GPIO_PIN_ARR[pin];
+    uint8_t  gpio_mode = GPIO_MODE_ARR[mode];
 
-    rcu_periph_clock_enable(rvstar_gpio_clk);
-    gpio_init(rvstar_gpio_port, rvstar_gpio_mode, GPIO_OSPEED_50MHZ,
-              rvstar_gpio_pin);
-    gpio_bit_write(rvstar_gpio_port, rvstar_gpio_pin, RESET);
+    rcu_periph_clock_enable(gpio_clk);
+    gpio_init(gpio_port, gpio_mode, GPIO_OSPEED_50MHZ, gpio_pin);
+    gpio_bit_write(gpio_port, gpio_pin, RESET);
 }
 
 void delay(unsigned long ms) {
