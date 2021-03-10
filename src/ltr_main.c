6c96ec31d9ad11cae6fe32ebb754c096e56879c3
myyerrol <myyerrol@126.com>
2021-03-10 13:22:36 +0800

    Update to 21.03.10

--- src/ltr_main.c
+++ src/ltr_main.c
@@ -4,9 +4,9 @@ int main(void) {
     // pinMode(11, OUTPUT);
 
     while (1) {
-        for (uint8_t i = 0; i < 255; i++) {
-            analogWrite(11, i);
-            delay(10);
-        }
+        delay(1000);
+        // printf("测试\n");
+        uint16_t a = analogRead(A0);
+        printf("value: %d\n", a);
     }
 }
