31d31594ea627043b2cee5a78cdc6f8233a35df8
myyerrol <myyerrol@126.com>
2021-03-13 10:08:32 +0800

    Update the lib

--- src/ltr_main.cpp
+++ src/ltr_main.cpp
@@ -1,16 +1,11 @@
 #include "ltr_main.h"
 
 int main(void) {
+    Wire.begin();
+
     while (1) {
         delay(1000);
         uint16_t a = analogRead(A0);
         printf("value: %d\n", a);
     }
-
-    // Wire.a = 1;
-    // Wire.b = 2;
-    // Wire.begin(100);
-    // Wire.begin();
-
-    Wire.begin(10);
 }
